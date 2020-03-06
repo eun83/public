ff61f576dbed122e35ac6ab1d934aff8676885aa
eun83 <nekoko83@gmail.com>
2020-03-06 22:36:25 +0900

    - 모바일 메뉴 일단락

--- playz/js/common.js
+++ playz/js/common.js
@@ -1,5 +1,6 @@
 $(function(){
-  
+
+  // web menu effect
   $(".menu>li").mouseover(function(){
       $(this).children("ul").stop().slideDown(200);
   });
@@ -7,7 +8,7 @@ $(function(){
       $(this).children("ul").stop().slideUp(200);
   });
 
-
+  // mobile menu open
   $('.btn-gnb').click(function() {
     $(".m-gnb, .page-cover, html").addClass("open");  
     window.location.hash = "#open"; 
@@ -18,6 +19,14 @@ $(function(){
       $(".m-gnb, .page-cover, html").removeClass("open");
     }
   };
+
+  // mobile menu toggle
+  $('.m-menu > li > a').click(function(){
+    var $target = $(this).parent().children('ul');
+    $('.m-menu > li > ul').hide();
+    $target.slideDown();
+    return false;
+  });
 
 });
 
